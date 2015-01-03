b8e671ec45ce411b7947065e50751000461336d6
Severin Wünsch <swuensch@gmail.com>
2015-01-03 19:11:44 +0100

    Code augeräumft und unnötige funktionen entfernt

--- src/Human.java
+++ src/Human.java
@@ -10,8 +10,7 @@ import sim.util.Int2D;
  */
 public class Human implements Steppable {
 
-	private Obstacle obst = null;
-	private Int2D oldPos;
+	private Int2D position;
 	private Boolean rightDirection;
 	public Boolean getRightDirection() {
 		return rightDirection;
@@ -19,7 +18,7 @@ public class Human implements Steppable {
 
 	private int rule;
 	public Human(Int2D pos, Boolean rightDirection, int rule) {
-		this.oldPos = pos;
+		this.position = pos;
 		this.rightDirection = rightDirection;
 		this.rule = rule;
 	}
@@ -29,55 +28,16 @@ public class Human implements Steppable {
 		// SimState state muss in Simulation gecastet werden (was es ja auch ist, da Simulation eine Subklasse von SimState ist).
 		Simulation simulation = (Simulation) state;
 		SparseGrid2D area = simulation.getArea();
-		
-		// Der Mensch wei� durch area.getObjectLocation(this), wo er sich befindet.
-		Int2D pos = area.getObjectLocation(this);
-		boolean shallDrop = false;
+
 		if(rule == 1){
-			
-			
-			
-			// In bag befinden sich alle Objekte, der aktuellen Position.
-			Bag bag = area.getObjectsAtLocation(pos);
-			for (Object object : bag) {
-				if (object instanceof Obstacle) {
-					// Wenn das Objekt nicht Null ist, nicht dem eigenen Objekt entspricht und nicht 
-					// bereits getragen wird, wird es fallen gelassen.
-					if (obst != null && !object.equals(obst)
-							&& !((Obstacle) object).isCarried()) {
-						shallDrop = true;
-						break;
-					} else {
-						//Das Objekt wird aufgenommen.
-						obst = (Obstacle) object;
-						obst.setCarried(true);
-						break;
-					}
-				}
-			}
+			position = rule1(area);
 		}
-		// Falls das Objekt abgelegt werden soll, springt die Ameise an die neue Position (old + stepSize),
-		// das Objekt wird an der "alten" Position abgelegt und nun nicht mehr getragen.
-		if (shallDrop) {
-			Int2D newPos = getNewPos(state, pos);
-			int x = (pos.x - oldPos.x) * Config.stepSize;
-			int y = (pos.y - oldPos.y) * Config.stepSize;
-			newPos = new Int2D(pos.x + x, pos.y + y);
-			area.setObjectLocation(this, newPos);
-			obst.setPosition(oldPos);
-			obst.setCarried(false);
-			obst = null;
-		} else {
-			// Falls kein Objekt abgelegt werden soll, "wigglet" die Ameise weiter.
-			Int2D newPos = getNewPos(state, pos);
-			area.setObjectLocation(this, newPos);
-			if (obst != null)
-				obst.setPosition(newPos);
-		}
-		oldPos = pos;
+
+		// Neue Position wird auf der area gestzt
+		area.setObjectLocation(this, this.position);
 	}
 	
-	public Int2D rule1 (Int2D pos, SparseGrid2D area){
+	public Int2D rule1 (SparseGrid2D area){
 		int nextFieldDiff;
 		if(rightDirection)
 			nextFieldDiff = 1;
@@ -139,39 +99,10 @@ public class Human implements Steppable {
 		return rule;
 	}
 	
-	public Obstacle getObst() {
-		return obst;
-	}
-
 	public Entropy getEntropy() {
 		return Entropy.getInstance();
 	}
 
-	private Int2D getNewPos(SimState state, Int2D pos) {
-		int rnd = (int) (state.random.nextDouble() * 4);
-		SparseGrid2D area = ((Simulation) state).getArea();
-		int x = pos.x;
-		int y = pos.y;
-		switch (rnd) {
-		case 0:
-			if (++x > area.getWidth() - 1)
-				x = 0;
-			break;
-		case 1:
-			if (++y > area.getHeight() - 1)
-				y = 0;
-			break;
-		case 2:
-			if (--x < 0)
-				x = area.getWidth() - 1;
-			break;
-		default:
-			if (--y < 0)
-				y = area.getHeight() - 1;
-			break;
-		}
-		return new Int2D(x, y);
-	}
 
 	public Int2D getPosition() {
 		//TODO soll Position zurückgeben
