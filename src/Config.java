74e40b7584ba23beb13c86ceb1d90b3ddddff2b2
natalia11 <natalia.odolad@gmail.com>
2015-01-03 20:09:16 +0100

    config

--- src/Config.java
+++ src/Config.java
@@ -1,8 +1,8 @@
 public final class Config {
 
-	public final static int numLeft = 50;
 	public final static int numRight = 50;
-
-	public final static int width = 50;
-	public final static int height = 50;
+	public final static int numLeft = 50;
+	
+	public final static int width = 100;
+	public final static int height = 20;
 }
