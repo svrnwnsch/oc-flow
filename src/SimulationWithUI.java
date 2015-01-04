7b5b6a7151b88d8664475d26d1d7905072d20845
Severin Wünsch <swuensch@gmail.com>
2015-01-04 16:55:38 +0100

    Farbe von Rule 3 auf Blau und Türkies gestzt

--- src/SimulationWithUI.java
+++ src/SimulationWithUI.java
@@ -135,6 +135,13 @@ public class SimulationWithUI extends GUIState {
 				else if (((Human) object).getRule()==0){
 					paint = new Color(255, 0, 0);
 				}
+				else if (((Human) object).getDirection()>0 && ((Human) object).getRule()==2) {
+					paint = new Color(51, 153, 255);
+				} 
+				else if (((Human) object).getDirection()<0 && ((Human) object).getRule()==2){
+					paint = new Color(51, 255, 255);
+				}
+
 				super.draw(object, graphics, info);
 			}
 		});
